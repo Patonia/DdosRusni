e6ceb802d358234d5e68856840ac43d4fa8857d3
Patonia <for_i@i.ua>
2022-04-01 18:41:27 +0300

    Create atack.py

---
+++ atack.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import socket,time,threading,sys,random,os,subprocess
+from os import getcwd
+
+url = "https://raw.githubusercontent.com/Patonia/Patonia/main/target.txt"
+directory = getcwd()
+filename = directory + 'target.txt'
+r = requests.get(url)
+
+f = open(filename,'w')
+f.write(r.content)
+
